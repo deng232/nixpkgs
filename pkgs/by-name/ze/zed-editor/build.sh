af81b46e6a9bb721466004499646d8a6e927ab16
deng232 <deng232@purdue.edu>
2026-05-31 23:13:56 -0400

    build success with big chunk of swap file

--- pkgs/by-name/ze/zed-editor/build.sh
+++ pkgs/by-name/ze/zed-editor/build.sh
@@ -1,13 +1,20 @@
+sudo fallocate -l 32G /var/tmp/nix-zed.swap
+sudo chmod 600 /var/tmp/nix-zed.swap
+sudo mkswap /var/tmp/nix-zed.swap
+sudo swapon -p 100 /var/tmp/nix-zed.swap
+
 sudo systemctl set-property --runtime nix-daemon.service \
   MemoryMax=20G \
-  MemorySwapMax=0 \
+  MemorySwapMax=32G \
   CPUQuota=1200% \
   AllowedCPUs=0-11
 
-
-  nix-build -j 1 -E '
+nix-build  -j 1 -E '
 let
   pkgs = import <nixpkgs> {};
 in
   pkgs.callPackage ./package.nix {}
 '
+
+sudo swapoff /var/tmp/nix-zed.swap
+sudo rm -f /var/tmp/nix-zed.swap
